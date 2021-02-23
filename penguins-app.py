ad0d5c9b65e77d2b97724527be234c3f53650fec
thedon122 <donovan122@yahoo.com>
2021-02-22 23:23:10 -0500

    collect user input

--- penguins-app.py
+++ penguins-app.py
@@ -15,3 +15,13 @@ st.sidebar.header('User Input Features')
 st.sidebar.markdown("""
 [Example CSV input file](https://raw.githubusercontent.com/dataprofessor/data/master/penguins_example.csv)
 """)
+
+# Collects user input features into dataframe
+uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["csv"])
+if uploaded_file is not None:
+    input_df = pd.read_csv(uploaded_file)
+else:
+    def user_input_features():
+        island = st.sidebar.selectbox('Island',('Biscoe','Dream','Torgersen'))
+        sex = st.sidebar.selectbox('Sex',('male','female'))
+        bill_length_mm = st.sidebar.slider('Bill length (mm)', 32.1,59.6,43.9)
