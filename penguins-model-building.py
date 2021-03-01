25058bc231312425e2d1feda1f2a7eceee06b546
thedon122 <donovan122@yahoo.com>
2021-02-28 19:29:35 -0500

    algorithm for data

--- penguins-model-building.py
+++ penguins-model-building.py
@@ -6,3 +6,14 @@ penguins = pd.read_csv('penguins_cleaned.csv')
 df = penguins.copy()
 target = 'species'
 encode = ['sex','island']
+
+for col in encode:
+    dummy = pd.get_dummies(df[col], prefix=col)
+    df = pd.concat([df,dummy], axis=1)
+    del df[col]
+
+target_mapper = {'Adelie':0, 'Chinstrap':1, 'Gentoo':2}
+def target_encode(val):
+    return target_mapper[val]
+
+df['species'] = df['species'].apply(target_encode)
