7489d1c6790adcf04fe3a0298088c102df41c448
thedon122 <donovan122@yahoo.com>
2021-02-28 19:22:06 -0500

    model building setup

--- penguins-model-building.py
+++ penguins-model-building.py
@@ -1,2 +1,8 @@
 import pandas as pd
 penguins = pd.read_csv('penguins_cleaned.csv')
+
+# Ordinal feature encoding
+# https://www.kaggle.com/pratik1120/penguin-dataset-eda-classification-and-clustering
+df = penguins.copy()
+target = 'species'
+encode = ['sex','island']
